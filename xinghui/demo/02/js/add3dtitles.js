3b0c313f265885e6ecba7ebd4a1d85da32afbb4b
杨亚男 <3288077283@qq.com>
2026-07-31 14:40:22 +0800

    开始在threejs里添加3dtitles

---
+++ xinghui/demo/02/js/add3dtitles.js
@@ -0,0 +1,7 @@
+
+import { TilesRenderer } from '../node_modules/3d-tiles-renderer/build/index.three.js';
+function add3dtitles() {
+    console.log('add3dtitles:::::yyn');
+    console.log(TilesRenderer);
+}
+export default add3dtitles;
---
+++ xinghui/demo/02/readMe.md
@@ -0,0 +1,3 @@
+## 解决threejs记载3dtiles问题
+
+- [3DTilesRendererJS 用户手册](https://github.com/NASA-AMMOS/3DTilesRendererJS/blob/master/USAGE.md)
